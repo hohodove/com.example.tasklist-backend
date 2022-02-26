7230c49d2085526a3255a6ea6b781faf9315145c
hohodove <hohodove@gmail.com>
2022-02-26 15:16:29 +0900

    jsonでhello wolrdを返却する機能について、テストコードを作成。

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,25 +1,40 @@
 package com.example
 
-import io.ktor.routing.*
 import io.ktor.http.*
-import io.ktor.jackson.*
-import com.fasterxml.jackson.databind.*
-import io.ktor.features.*
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
 import kotlin.test.*
 import io.ktor.server.testing.*
 import com.example.infrastructure.framework.*
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 
 class ApplicationTest {
     @Test
-    fun testRoot() {
+    fun `ルートパスにアクセスした場合、Hello World!がテキストで返却される`() {
         withTestApplication({ configureRouting() }) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("Hello World!", response.content)
+                assertEquals("Hello World", response.content)
             }
         }
     }
+
+    data class HelloJson(val hello: String)
+
+    @Test
+    fun `HelloJsonパスにアクセスした場合、変数名：hello、値：worldのjsonが返却される`() {
+        withTestApplication({
+            configureRouting()
+            configureSerialization()
+        }) {
+            handleRequest(HttpMethod.Get, "/helloJson").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+
+                val mapper = jacksonObjectMapper()
+                val helloJson = mapper.readValue<HelloJson>(response.content!!)
+                assertEquals("world", helloJson.hello)
+            }
+        }
+    }
+
+
 }
