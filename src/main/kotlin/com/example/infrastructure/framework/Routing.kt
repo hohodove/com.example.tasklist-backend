81dd7cbf4ba418fe9f798a86e77a7be4be621173
hohodove <hohodove@gmail.com>
2022-02-26 15:14:58 +0900

    import文を整理。

--- src/main/kotlin/com/example/infrastructure/framework/Routing.kt
+++ src/main/kotlin/com/example/infrastructure/framework/Routing.kt
@@ -2,7 +2,6 @@ package com.example.infrastructure.framework
 
 import io.ktor.routing.*
 import io.ktor.application.*
-import io.ktor.response.*
 import com.example.controller.*
 
 fun Application.configureRouting() {
