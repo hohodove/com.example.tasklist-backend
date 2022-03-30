52d2da411728b4d0e5e441c2bdf1ee53a8114cdc
hohodove <hohodove@gmail.com>
2022-03-30 10:13:43 +0900

    コメントの適応範囲がわかりやすいよう、改行を追加。

--- src/main/kotlin/com/example/infrastructure/framework/Koin.kt
+++ src/main/kotlin/com/example/infrastructure/framework/Koin.kt
@@ -10,7 +10,8 @@ fun Application.configureKoin() {
     install(Koin) {
         //KoinのIssue #1188の通り、Ktor 1.6.0以降でKoinにてNoSuchMethodError例外が発生するため、
         // workaroundとして、ロガーのログレベルをERRORで設定している。
-        slf4jLogger(level = Level.ERROR)  //
+        slf4jLogger(level = Level.ERROR)
+
         modules(taskRepositoryModule)
     }
 }
