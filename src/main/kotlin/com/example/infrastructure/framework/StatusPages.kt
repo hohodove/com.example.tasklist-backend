4d2dddb1636edee1b3fadcb2c1fea9ca31758b28
hohodove <hohodove@gmail.com>
2022-04-15 17:07:59 +0900

    IllegalArgumentException発生時、HTTP Status 400とエラーメッセージをレスポンスするよう修正。 ・取得や更新、削除対象のタスクが存在しない場合など

--- src/main/kotlin/com/example/infrastructure/framework/StatusPages.kt
+++ src/main/kotlin/com/example/infrastructure/framework/StatusPages.kt
@@ -1,5 +1,6 @@
 package com.example.infrastructure.framework
 
+import com.example.controller.response.ErrorMessageResponse
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.http.*
@@ -7,6 +8,15 @@ import io.ktor.response.*
 
 fun Application.configureStatusPages() {
     install(StatusPages) {
+
+        exception<IllegalArgumentException> { cause ->
+            call.respond(
+                HttpStatusCode.BadRequest,
+                ErrorMessageResponse(HttpStatusCode.BadRequest.value.toString(), cause.message!!)
+            )
+            throw cause
+        }
+
         exception<Throwable> { cause ->
             call.respond(HttpStatusCode.InternalServerError, "Internal Server Error")
             throw cause
